6a04166479f7fd18f85b28d97137b73d9f9916a6
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-01-06 20:59:55 -0800

    Found an error

---
+++ db/migrate/20200107044903_billboards.rb
@@ -0,0 +1,8 @@
+class Billboards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :billboards_songs, id: false do |t|
+      t.belongs_to :billboard, index: true
+      t.belongs_to :song, index: true
+    end
+  end
+end
