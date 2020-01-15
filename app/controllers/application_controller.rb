5554756d3e7f4128173cd9156af7827534fa19d2
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-01-14 17:34:43 -0800

    Updating master to then start on back on develop branch to figure out some issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_view_count_cookie
+  private
+  def set_view_count_cookie
+    if cookies[:views]
+      cookies[:views] = cookies[:views].to_i + 1
+    else
+      cookies[:views] = 0
+    end
+  end
 end
--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -69,6 +69,6 @@ class BillboardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def billboard_params
-      params.fetch(:billboard, {})
+      params.require(:billboard).permit(:title, :country)
     end
 end
